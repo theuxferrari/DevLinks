f4b964b14aee3cf62d407307814eecc419964a1f
Matheus P. Ferreira <theuxferrari@gmail.com>
2023-08-23 02:05:22 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('light')
+}
+
+/* 
+if(body.classList.contains('light')) {
+    body.classList.remove('light');
+}else {
+    body.classList.add('light');
+}
+*/
